188c00aca7e59398e4783b00a6fbf9ed20af07e0
Mahmoud Elgendi <Mahmoud.E@miviewis.com>
2026-07-30 00:34:50 -0500

    Map & distructor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ReactImage from "./assets/react-core-concepts.png";
 import ComponentsImage from "./assets/components.png";
 const reactDescriptions = ["Fundamentals", "Crusial", "Core"];
+import { CORE_CONCEPTS } from "./data.js";
 function GetRandonInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
@@ -19,12 +20,14 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+// destructor instead of props
+// property names must match the object keys
+function CoreConcept({ title, description, image }) {
   return (
     <li>
-      <img src={props.image} alt="..." />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt="..." />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -36,13 +39,31 @@ function App() {
       <main>
         <section id="core-concepts">
           <ul>
-            <CoreConcept
-              title="Components"
-              description="Reusable building blocks of the UI."
-              image={ComponentsImage}
+            {/* <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept title="props" />
-            <CoreConcept />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            /> */}
+
+            {/* <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+
+            {/* alternative way using map function  */}
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConcept {...concept} />
+            ))}
           </ul>
         </section>
         <h2>Time to get started!</h2>
