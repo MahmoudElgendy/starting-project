85c51330efd568bffeb1eb1e89a927b304eab9be
Mahmoud Elgendi <Mahmoud.E@miviewis.com>
2026-07-29 23:57:17 -0500

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ReactImage from "./assets/react-core-concepts.png";
+import ComponentsImage from "./assets/components.png";
 const reactDescriptions = ["Fundamentals", "Crusial", "Core"];
 function GetRandonInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -17,11 +18,33 @@ function Header() {
     </header>
   );
 }
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt="..." />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="Reusable building blocks of the UI."
+              image={ComponentsImage}
+            />
+            <CoreConcept title="props" />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
